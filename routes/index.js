a63a1852db869807c6fe691a3ed341c22911c0d9
Prudence Wambui <139547435+Wambuuyy@users.noreply.github.com>
2024-09-04 10:28:02 +0300

    Create index.js

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import AppController from '../controllers/AppController.js';
+import UsersController from '../controllers/UsersController.js';
+import AuthController from '../controllers/AuthController.js';
+import FilesController from '../controllers/FilesController.js';
+
+const router = Router();
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', AuthController.getMe);
+router.post('/files', FilesController.postUpload);
+
+export default router;
